2cbf9ca0fcae2ef25bfdc0dd3bf0dd5033413f53
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-02-20 17:59:17 +0500

    fixed dependency issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,19 +15,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
-      },
-    },
     {
       resolve: "gatsby-source-graphql",
       options: {
